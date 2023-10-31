f82d702e154ef66c15c4cea7e157f1bc80b00d91
Apk <146819953+7NRVHXY@users.noreply.github.com>
2023-10-31 23:38:58 +0400

    created6

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿namespace switchcaseseasons
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string season = "winter";
+            switch (season)
+            {
+                case "winter":
+                    Console.WriteLine("november , december , january");
+                    break;
+                case "spring":
+                    Console.WriteLine("february , march , april");
+                    break;
+                case "summer":
+                    Console.WriteLine("may , june , july");
+                    break;
+                case "autumn":
+                    Console.WriteLine("august , september , october");
+                    break;
+
+
+
+
+
+            }
+        }
+    }
+}
